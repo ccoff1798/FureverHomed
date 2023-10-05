718d252abb97aa4d2e7cd661d05540127e3fbc32
ccoff1798 <135388607+ccoff1798@users.noreply.github.com>
2023-10-05 12:04:16 -0600

    token Fetcher helper added

---
+++ helpers/testrun.js
@@ -0,0 +1,4 @@
+const TokenFetcher = require('./tokenFetcher'); // Assuming TokenFetcher is in a separate file
+
+const tokenFetcherInstance = new TokenFetcher();
+ console.log(tokenFetcherInstance)
---
+++ helpers/tokenFetcher.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+require('dotenv').config();
+
+class TokenFetcher {
+  constructor() {
+    console.log('token working');
+    const clientId = process.env.API_KEY;
+    const clientSecret = process.env.API_SECRET;
+
+    const tokenUrl = "https://api.petfinder.com/v2/oauth2/token";
+
+    const data = new URLSearchParams();
+    data.append("grant_type", "client_credentials");
+    data.append("client_id", clientId);
+    data.append("client_secret", clientSecret);
+
+    axios.post(tokenUrl, data)
+      .then((response) => {
+        if (response.status === 200) {
+          const newAccessToken = response.data.access_token;
+          console.log(newAccessToken);
+        } else {
+          console.error(`Failed to refresh access token. Status code: ${response.status}`);
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred while refreshing the token:", error);
+      });
+  }
+}
+module.exports = TokenFetcher;
