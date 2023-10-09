327d50f4c40e7519dd27a59caeaa4eba657a356a
ccoff1798 <135388607+ccoff1798@users.noreply.github.com>
2023-10-09 12:13:04 -0600

    homepage logic and passthrough working, useable as template for more complicated fetches

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,32 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+const HomePageLogic = require('../frontendlogic/homepage');
 
-// users not logged in will be sent to home page to login. 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-// code below is getting user data from user logged in password is excluded above and assigned to variable 'users'
-    const users = userData.map((project) => project.get({ plain: true }));
-// this is rendering home page with user logged in data previously gotten above.
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// Prevent non logged in users from viewing the homepage //withAuth
+router.get('/', async (req, res) => {
+//   try {
+  const homePageLogic = new HomePageLogic()
+  const homePageFetcher = await homePageLogic.initializeFetcher
+  homePageFetcher()
+  res.render('homepage')
+
+  
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
+// // code below is getting user data from user logged in password is excluded above and assigned to variable 'users'
+//     const users = userData.map((project) => project.get({ plain: true }));
+// // this is rendering home page with user logged in data previously gotten above.
+//     res.render('homepage', {
+//       users,
+//       // Pass the logged in flag to the template
+//       // logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
 });
 
 router.get('/login', (req, res) => {
--- helpers/testrun.js
+++ helpers/testrun.js
@@ -1,20 +1,34 @@
-
-async function fetchData() {
+async function fetchData(location) {
     const TokenFetcher = require('./tokenFetcher');
 
     try {
         const tokenFetcherInstance = new TokenFetcher();
         const tokenFetched = await tokenFetcherInstance.fetcher()
-        const url = 'https://api.petfinder.com/v2/animals/'
-        console.log(urlHeaders)
+        const url = `https://api.petfinder.com/v2/animals/?location=${location}`
         const fetchRequest = await fetch(url, {
             headers: {
                 'Authorization': 'Bearer ' + tokenFetched
             }
         })
-
+        const animalArray = []
         const data = await fetchRequest.json()
-        console.log(data)
+        //pushing data into an array
+        for(let i = 0; i < data.animals.length; i++){
+            // animalArray.push('id:',data.animals[i].id)
+            // animalArray.push('name:',data.animals[i].name)
+            // animalArray.push('type:', data.animals[i].type)
+            const animalObject ={
+                id : data.animals[i].id,
+                name : data.animals[i].name,
+                type : data.animals[i].type,
+                photo : data.animals[i].primary_photo_cropped,
+                url : data.animals[i].url
+            }
+            animalArray.push(JSON.stringify(animalObject))
+
+        }
+        console.log(animalArray)
+        
 
     }
     catch (error) {
@@ -22,4 +36,5 @@ async function fetchData() {
     }
 };
 
-console.log(fetchData())
+
+module.exports = fetchData
