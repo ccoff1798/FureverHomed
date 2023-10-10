83c53484239859e0e6e2fbc8727267ec96e01ac5
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-10-09 23:09:01 -0400

    adding fetches for Type, Breed, Location & Rescue. Only Type wokring

--- controllers/api/fetchRoutes.js
+++ controllers/api/fetchRoutes.js
@@ -1,6 +1,6 @@
-let type = "Dog" 
+let type = "Dog"
 // GET https://api.petfinder.com/v2/types/{type}
-let breed = "French Bulldog" 
+let breed = "French Bulldog"
 // GET https://api.petfinder.com/v2/types/${type}/breeds/${breed}
 let location = "Denver"
 // GET https://api.petfinder.com/v2/animals/locations/${location}
@@ -9,13 +9,13 @@ let rescue = "DDFL"
 
 
 
-async function fetchData() {
+async function fetchTypes() {
     const TokenFetcher = require('../../helpers/tokenFetcher');
 
     try {
         const tokenFetcherInstance = new TokenFetcher();
         const tokenFetched = await tokenFetcherInstance.fetcher()
-        const url = `https://api.petfinder.com/v2/animals/types/${type}`
+        const url = `https://api.petfinder.com/v2/types/${type}`
         // ${breed}/${location}/${rescue}`
         console.log(url)
         const fetchRequest = await fetch(url, {
@@ -23,27 +23,111 @@ async function fetchData() {
                 'Authorization': 'Bearer ' + tokenFetched
             }
         })
-        .then(function(response){
-            return response.json
-    })
-    .then(function(data){
-        // sortByBreed(data)
-        console.log(data)
-    }
-    )
-        const data = await fetchRequest.json()
-        // const data = await return fetchRequest.json()
-        console.log(data)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                // sortByBreed(data)
+                console.log(data)
+            })
 
     }
     catch (error) {
         console.log(error)
     }
 };
+async function fetchBreeds() {
+    const TokenFetcher = require('../../helpers/tokenFetcher');
+
+    try {
+        const tokenFetcherInstance = new TokenFetcher();
+        const tokenFetched = await tokenFetcherInstance.fetcher()
+        const url = `https://api.petfinder.com/v2/breeds/${breed}`
+        // ${breed}/${location}/${rescue}`
+        console.log(url)
+        const fetchRequest = await fetch(url, {
+            headers: {
+                'Authorization': 'Bearer ' + tokenFetched
+            }
+        })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                // sortByBreed(data)
+                console.log(data)
+            })
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+};
+async function fetchLocation() {
+    const TokenFetcher = require('../../helpers/tokenFetcher');
+
+    try {
+        const tokenFetcherInstance = new TokenFetcher();
+        const tokenFetched = await tokenFetcherInstance.fetcher()
+        const url = `https://api.petfinder.com/v2/locations/${location}`
+        // ${breed}/${location}/${rescue}`
+        console.log(url)
+        const fetchRequest = await fetch(url, {
+            headers: {
+                'Authorization': 'Bearer ' + tokenFetched
+            }
+        })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                // sortByBreed(data)
+                console.log(data)
+            })
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+};
+
+
+
+
+async function fetchRescue() {
+    const TokenFetcher = require('../../helpers/tokenFetcher');
+try {
+    const tokenFetcherInstance = new TokenFetcher();
+    const tokenFetched = await tokenFetcherInstance.fetcher()
+    const url = `https://api.petfinder.com/v2/organizations/${rescue}}`
+    // ${breed}/${location}/${rescue}`
+
+    console.log(url)
+    const fetchRequest = await fetch(url, {
+        headers: {
+            'Authorization': 'Bearer ' + tokenFetched
+        }
+    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            // sortByBreed(data)
+            console.log(data)
+        })
+
+}
+catch (error) {
+    console.log(error)
+}
+};
 // function sortByBreed(data)
 //     data.forEach(element => {
-        
+
 //     });
 
 
-fetchData()
+fetchTypes()
+fetchBreeds()
+fetchLocation()
+fetchRescue()
