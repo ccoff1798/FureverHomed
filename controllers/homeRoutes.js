5f3b0a7a1cb0a5ac86bc0f394637df3ac353eb8c
ccoff1798 <135388607+ccoff1798@users.noreply.github.com>
2023-10-09 21:36:31 -0600

    uncommented login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,32 +4,29 @@ const withAuth = require('../utils/auth');
 const HomePageLogic = require('../public/frontendlogic/homepage');
 
 // Prevent non logged in users from viewing the homepage //withAuth
-router.get('/', async (req, res) => {
-//   try {\
-  const homePageLogic = new HomePageLogic()
-  const homePageFetcher = await homePageLogic.initializeFetcher
-  homePageFetcher()
-  
-  res.render('homepage')
-  
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const homePageLogic = new HomePageLogic();
+    const homePageFetcher = await homePageLogic.initializeFetcher(); // Assuming this is a function
+    homePageFetcher();
 
-  
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-// // code below is getting user data from user logged in password is excluded above and assigned to variable 'users'
-//     const users = userData.map((project) => project.get({ plain: true }));
-// // this is rendering home page with user logged in data previously gotten above.
-//     res.render('homepage', {
-//       users,
-//       // Pass the logged in flag to the template
-//       // logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      // Pass the logged in flag to the template
+      // logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 // landing page that does not require for anyone to be logged in
 // router.get('/', (req,res)=> {
 //   res.render("homepage", {
